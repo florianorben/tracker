6545b554b8ae8291c6a2106b0282eedb2007a823
Florian Orben <florian.orben@schmetterling.de>
2018-04-27 23:31:44 +0200

    add -s flag to status command to shorten output

--- cmd/status.go
+++ cmd/status.go
@@ -16,12 +16,22 @@ var statusCmd = &cobra.Command{
 
 	$ tracker start test +foo
 	$ tracker status
-	Project test [foo] started just now (2016-04-25 21:38:00 +0200 CEST)`,
+	Project test [foo] started just now (2016-04-25 21:38:00 +0200 CEST)
+    
+    You can display a shorter version of status with the -short flag
+    
+    Example:
+    
+    $ tracker start test +foo
+    $ tracker status
+    Project test started just now`,
 	Run: status,
 }
 
 func init() {
 	RootCmd.AddCommand(statusCmd)
+
+	statusCmd.Flags().StringP("short", "s", "", "Truncated output")
 }
 
 func status(cmd *cobra.Command, args []string) {
@@ -42,12 +52,16 @@ func status(cmd *cobra.Command, args []string) {
 			formattedTags = " " + formattedTags
 		}
 
-		fmt.Printf(
-			"Project %s%s started %s (%s)\n",
-			activeFrame.FormattedProject(),
-			formattedTags,
-			activeFrame.RelativeTime(),
-			activeFrame.Start,
-		)
+        if cmd.Flag("short").Value.String() != "" {
+            fmt.Printf("Project %s started %s\n", activeFrame.FormattedProject(), activeFrame.RelativeTime())
+        } else {
+		    fmt.Printf(
+		        "Project %s%s started %s (%s)\n",
+			    activeFrame.FormattedProject(),
+		        formattedTags,
+		        activeFrame.RelativeTime(),
+		        activeFrame.Start,
+		    )
+        }
 	}
 }
